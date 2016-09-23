1cacc83c6672e855b5aa4aeb34035d7fb8483816
XETHseth <XETHseth@android-c975387de38d8db2.cmua.dom>
2016-09-23 16:51:00 +0930

    v23

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -17,6 +17,7 @@ public class GitTest {
     public static void main(String[] args) {
         // TODO code application logic here
         //I change change change.
+        //at this moment
     }
     
 }
